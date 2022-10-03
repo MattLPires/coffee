52cdef4de913ea257afdac26eefc0a8c84ef8cac
Matheus Pires <matheus.h98@hotmail.com.br>
2022-10-03 09:48:02 -0300

    main

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 function imgSlider(anything){
   document.querySelector('.prod').src = anything;
 }
-
-function clear() {
-  document.getElementById('nameid').value='';
-  document.getElementById('phoneid').value='';
-  document.getElementById('emailid').value='';
-  document.getElementById('assuntoid').value='';
-  document.getElementById('msgid').value='';
-}
-
